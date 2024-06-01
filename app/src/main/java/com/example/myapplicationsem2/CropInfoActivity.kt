a9b10d1b4bb52329dbde264207b4e3a1f8ee7491
Samrendra Pratap Singh <153061395+solo-source@users.noreply.github.com>
2024-06-01 16:37:23 +0530

    Implemented recycler view in CropInfoActivity

--- app/src/main/java/com/example/myapplicationsem2/CropInfoActivity.kt
+++ app/src/main/java/com/example/myapplicationsem2/CropInfoActivity.kt
@@ -1,20 +1,108 @@
 package com.example.myapplicationsem2
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplicationsem2.databinding.ActivityCropInfoBinding
+import com.example.myapplicationsem2.databinding.ItemCropBinding
 
 class CropInfoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCropInfoBinding
+    private val cropList = generateCropList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_crop_info)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityCropInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set up back button click listener
+        binding.backButton.setOnClickListener {
+            onBackPressed()
+        }
+
+        // Set up RecyclerView
+        binding.cropListRecyclerView.adapter = CropListAdapter(cropList)
+    }
+
+    private fun generateCropList(): List<Crop> {
+        // Generate sample crop data
+        // Replace this with your actual crop data
+        val crops = mutableListOf<Crop>()
+        crops.add(
+            Crop(
+                "Crop 1",
+                "What is Lorem Ipsum?\n" +
+                        "\n" +
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                R.drawable.defaultcrop128
+            )
+        )
+        crops.add(
+            Crop(
+                "Crop 2",
+                "What is Lorem Ipsum?\n" +
+                        "\n" +
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                R.drawable.defaultcrop128
+            )
+        )
+        crops.add(
+            Crop(
+                "Crop 3",
+                "What is Lorem Ipsum?\n" +
+                        "\n" +
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                R.drawable.defaultcrop128
+            )
+        )
+        // Add more crops as needed
+        return crops
+    }
+
+    data class Crop(val name: String, val description: String, val imageResId: Int)
+
+    inner class CropListAdapter(private val crops: List<Crop>) :
+        RecyclerView.Adapter<CropListAdapter.CropViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CropViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val itemBinding = ItemCropBinding.inflate(inflater, parent, false)
+            return CropViewHolder(itemBinding)
+        }
+
+        override fun onBindViewHolder(holder: CropViewHolder, position: Int) {
+            val crop = crops[position]
+            holder.bind(crop)
+        }
+
+        override fun getItemCount(): Int {
+            return crops.size
+        }
+
+        inner class CropViewHolder(private val itemBinding: ItemCropBinding) :
+            RecyclerView.ViewHolder(itemBinding.root), View.OnClickListener {
+
+            init {
+                itemBinding.root.setOnClickListener(this)
+            }
+
+            fun bind(crop: Crop) {
+                itemBinding.cropImage.setImageResource(crop.imageResId)
+                itemBinding.cropName.text = crop.name
+                itemBinding.cropDescription.text = crop.description
+                // Set visibility of detailed information initially to GONE
+                itemBinding.detailedInfo.visibility = View.GONE
+            }
+
+            override fun onClick(view: View?) {
+                // Toggle visibility of detailed information on item click
+                val isVisible = itemBinding.detailedInfo.visibility == View.VISIBLE
+                itemBinding.detailedInfo.visibility = if (isVisible) View.GONE else View.VISIBLE
+            }
         }
     }
 }
